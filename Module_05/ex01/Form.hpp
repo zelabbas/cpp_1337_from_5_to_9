3218b6a24c0b5746af524466decf668f1241ab19
zelabbas <zakariaelabbassi2002@gmail.com>
2024-07-16 11:33:59 +0100

    define form class

---
+++ Module_05/ex01/Form.hpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/13 16:59:36 by zelabbas          #+#    #+#             */
+/*   Updated: 2024/07/16 11:22:35 by zelabbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include "Bureaucrat.hpp"
+class Bureaucrat;
+
+class Form
+{
+	private:
+		const std::string	name;
+		bool				_signed;
+		const int			gradeRequiredtoSign;
+		const int			gradeRequiredtoExcute;
+		
+	public:
+		Form();
+		Form(const std::string&, int, int);
+		Form(const Form&);
+		~Form();
+
+		Form& operator= (const Form&);
+
+		// getters
+		const std::string&	getName(void) const;
+		bool				isSigned(void) const;
+		int					getGradeRequiredtoSign(void) const;
+		int					getGradeRequiredtoExcute(void) const;
+		
+		void	beSigned(const Bureaucrat&);
+		
+		// class Exception
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				const char* what() const throw()
+				{
+					return "Form: Grade too high!";
+				}
+		};
+		
+		class GradeTooLowException : public std::exception 
+		{
+			public:
+				const char * what() const throw()
+				{
+					return "Form: Grade too low";
+				}
+		};
+};
+
+std::ostream& operator << (std::ostream& out, const Form& obj);
