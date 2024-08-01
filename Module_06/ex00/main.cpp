0b0fa3b961eabe4c34b8096c019da50c1404d3f4
zelabbas <zakariaelabbassi2002@gmail.com>
2024-08-01 22:27:16 +0100

    a small program to test the class

---
+++ Module_06/ex00/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/19 16:24:22 by zelabbas          #+#    #+#             */
+/*   Updated: 2024/08/01 22:26:22 by zelabbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+
+int main(int ac, char **av)
+{
+	(void)av;
+
+	if (ac != 2)
+	{
+		std::cerr << "Error usage: ./scalar arg" << std::endl;
+		return (1);
+	}
+	ScalarConverter::convert(static_cast<std::string>(av[1]));
+	return (0);
+}
