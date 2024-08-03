a419aeeab24102da7e1365c81d9a2545e885c851
zelabbas <zakariaelabbassi2002@gmail.com>
2024-08-03 20:40:13 +0100

    define the Data struct

---
+++ Module_06/ex01/Data.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Data.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/03 20:38:28 by zelabbas          #+#    #+#             */
+/*   Updated: 2024/08/03 20:38:37 by zelabbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+typedef	struct s_data {
+	int		intValue;
+	float	floatValue;
+	char	charValue;
+	bool	boolValue;	
+}	Data;
