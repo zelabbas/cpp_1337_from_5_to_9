5ff27179ea9a277de1d9537cbd16c1e784d028cf
zelabbas <zakariaelabbassi2002@gmail.com>
2024-08-05 12:19:59 +0100

    creat a small program to test

---
+++ Module_07/ex00/main.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/05 11:59:29 by zelabbas          #+#    #+#             */
+/*   Updated: 2024/08/05 12:14:15 by zelabbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+#include <iostream>
+
+
+int main(int ac, char **av)
+{
+	(void)av;
+
+	if (ac != 1)
+	{
+		std::cerr << "Error usage: ./whatever!" << std::endl;
+		return (1);
+	}
+
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
