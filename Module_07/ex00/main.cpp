ce2413ffb7a38bf704f27973fb957a7965635383
zelabbas <zakariaelabbassi2002@gmail.com>
2024-08-06 19:59:26 +0100

    update in main program

--- Module_07/ex00/main.cpp
+++ Module_07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 11:59:29 by zelabbas          #+#    #+#             */
-/*   Updated: 2024/08/05 12:14:15 by zelabbas         ###   ########.fr       */
+/*   Updated: 2024/08/06 19:59:05 by zelabbas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,13 @@ int main(int ac, char **av)
 
 	int a = 2;
 	int b = 3;
-	::swap( a, b );
+	swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << max( a, b ) << std::endl;
 	std::string c = "chaine1";
 	std::string d = "chaine2";
-	::swap(c, d);
+	swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
