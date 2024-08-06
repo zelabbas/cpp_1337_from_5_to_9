4bae67f4a64510b14f18f028de362906925e474e
zelabbas <zakariaelabbassi2002@gmail.com>
2024-08-06 18:55:33 +0100

    small update in main program

--- Module_07/ex01/main.cpp
+++ Module_07/ex01/main.cpp
@@ -6,12 +6,27 @@
 /*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 12:21:27 by zelabbas          #+#    #+#             */
-/*   Updated: 2024/08/05 12:52:05 by zelabbas         ###   ########.fr       */
+/*   Updated: 2024/08/06 18:49:40 by zelabbas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 #include <iostream>
+
+
+void	plusOne(int &nbr)
+{
+	nbr += 1;
+}
+
+void	_toUpper(std::string& str)
+{
+	for (size_t i = 0; i < str.size(); i++)
+	{
+		str[i] = std::toupper(static_cast<int>(str[i]));
+	}
+	
+}
 
 int main(int ac, char **av)
 {
@@ -22,18 +37,21 @@ int main(int ac, char **av)
 		std::cerr << "Error usage: ./iter" << std::endl;
 		return (1);
 	}
-
-	int a[5] = { 10, 50, 30, 40, 20 };
-	size_t n = sizeof(a) / sizeof(a[0]);
-	iter(a, n, swap <int>);
-	for (size_t i = 0; i < n; i++)
-		std::cout << a[i] << " ";
-	std::cout << std::endl;
-
-	std::string s[5] = { "hey", "hello", "who", "son", "clear" };
-	size_t len = sizeof(s) / sizeof(s[0]);
-	iter(s, len, swap <std::string>);
-	for (size_t i = 0; i < len; i++)
-		std::cout << s[i] << " ";
-	std::cout << std::endl;
+	try 
+	{
+		int a[5] = { 10, 50, 30, 40, 20 };
+		iter(a, 5, plusOne);
+		for (size_t i = 0; i < 5; i++)
+			std::cout << a[i] << " ";
+		std::cout << std::endl;
+	
+		std::string s[5] = { "hey", "hello", "who", "son", "clear" };
+		iter(s, 5, _toUpper);
+		for (size_t i = 0; i < 5; i++)
+			std::cout << s[i] << " ";
+		std::cout << std::endl;
+	}catch(const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 }
