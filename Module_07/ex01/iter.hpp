840bc3726d1b4bafb36eb8d1d367cfe67d72a949
zelabbas <zakariaelabbassi2002@gmail.com>
2024-08-06 18:55:18 +0100

    update the tamplate function

--- Module_07/ex01/iter.hpp
+++ Module_07/ex01/iter.hpp
@@ -6,26 +6,19 @@
 /*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 12:23:58 by zelabbas          #+#    #+#             */
-/*   Updated: 2024/08/05 12:46:16 by zelabbas         ###   ########.fr       */
+/*   Updated: 2024/08/06 18:50:00 by zelabbas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once 
 #include <cstddef>
-template <typename T> void swap(T &a, T &b)
+#include <stdexcept> 
+
+template <typename T, typename F> 
+void iter(T* a, std::size_t length , void (*fun)(F&))
 {
-	T c;
-
-	c = a;
-	a = b;
-	b = c;
-}
-
-
-template <typename T, typename Func> void iter(T* a, size_t length , Func fun)
-{
-	for (size_t i = 0; i < length - 1; i++)
-		for (size_t j = length - 1; i < j; j--)
-			if (a[j] < a[j - 1])
-				fun(a[j], a[j - 1]);
+	if (!a || !fun)
+		throw std::invalid_argument("Null pointer exception");
+	for (size_t i = 0; i < length; i++)
+		fun(a[i]);
 }
